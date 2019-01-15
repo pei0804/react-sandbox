53ec41f157bf10ffe65a0e90d03619923c54afef
pei0804 <peeeei0804@gmail.com>
2019-01-15 22:48:15 +0900

    up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//           Hello, World!
+//       </div>
+//     );
+//   }
+// }
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <p>Hello, World!</p>
-      </div>
+        React.createElement('div',null, 'helloworld')
     );
   }
 }
