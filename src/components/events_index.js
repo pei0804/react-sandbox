0db95c869af126e1536d3c2772e28d586c4f1fc0
pei0804 <peeeei0804@gmail.com>
2019-01-17 23:53:33 +0900

    update delete

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {readEvents} from "../action";
+import {readEvents} from "../actions";
 import _ from 'lodash'
 import {Link} from 'react-router-dom'
 
@@ -13,7 +13,7 @@ class EventsIndex extends Component {
         return _.map(this.props.events, event => (
             <tr key={event.id}>
                 <td>{event.id}</td>
-                <td>{event.title}</td>
+                <td><Link to={`/events/${event.id}`}>{event.title}</Link></td>
                 <td>{event.body}</td>
             </tr>
         ))
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,11 +1,19 @@
 import _ from 'lodash'
-import {READ_EVENTS} from "../action";
+import {DELETE_EVENT, READ_EVENTS, READ_EVENT, UPDATE_EVENT, CREATE_EVENT} from "../actions";
 
-export default (state = {}, action) => {
+export default (events = {}, action) => {
     switch (action.type) {
+        case  CREATE_EVENT:
+        case  READ_EVENT:
+        case  UPDATE_EVENT:
+            const data = action.response.data
+            return {...events, [data.id]: data}
         case  READ_EVENTS:
             return _.mapKeys(action.response.data, 'id')
+        case  DELETE_EVENT:
+            delete events[action.id]
+            return events
         default:
-            return state
+            return events
     }
 }
